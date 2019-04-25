a8b522a8d8c95eb379c3da50a846d01cbe0ce1eb
Glendon Philipp <gpbaculio@gmail.com>
2019-04-26 01:16:13 +0800

    showing y axis

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,9 @@ interface AppState {
   xMaxDate: Date | undefined;
   xMinDate: Date | undefined;
   error: string | null;
-  data: string[][] | null;
+  data: [string, number][] | null;
+  gdp: number[] | null;
+  gdpMax: number | undefined;
 }
 
 class App extends Component<AppProps, AppState> {
@@ -21,31 +23,39 @@ class App extends Component<AppProps, AppState> {
       xMaxDate: new Date(Date.now()),
       xMinDate: new Date(Date.now()),
       error: null,
-      data: null
+      data: null,
+      gdp: null,
+      gdpMax: 0
     };
   }
   componentDidMount = async () => {
     try {
-      interface res {
-        data: string[][];
-      }
-      const { data }: res = await axios.get(url).then(({ data }) => data);
+      const {
+        data
+      }: {
+        data: [string, number][];
+      } = await axios.get(url).then(({ data }) => data);
       // data is array of arrays, data on first index of an array is a date
-      const yearsDate: string[] = data.map(([item]) => item);
+      const yearsDate: string[] = data.map(([year]) => year);
       // x-axis max
       const xMaxDate = new Date(max(yearsDate) as string);
       // set to the end of quarters
       xMaxDate.setMonth(xMaxDate.getMonth() + 3); // month starts at 0:jan
       // x-axis min
       const xMinDate = new Date(min(yearsDate) as string);
-      this.setState({ xMaxDate, xMinDate, data });
+
+      const gdp: number[] = data.map(([, gdp]) => gdp);
+
+      const gdpMax = max(gdp);
+
+      this.setState({ xMaxDate, xMinDate, data, gdp, gdpMax });
       this.createBarChart();
     } catch (error) {
       this.setState({ error });
     }
   };
   createBarChart = () => {
-    const { data, xMinDate, xMaxDate } = this.state;
+    const { data, xMinDate, xMaxDate, gdpMax } = this.state;
     const yMargin = 40,
       width = 800,
       height = 400,
@@ -69,7 +79,7 @@ class App extends Component<AppProps, AppState> {
       .text('More Information: http://www.bea.gov/national/pdf/nipaguid.pdf')
       .attr('class', 'info');
 
-    // handle bottom axis
+    // handle bottom x axis
     if (xMinDate && xMaxDate) {
       // using scaleTime because of date
       const xScale = scaleTime()
@@ -83,6 +93,20 @@ class App extends Component<AppProps, AppState> {
         .call(xAxis)
         .attr('id', 'x-axis')
         .attr('transform', 'translate(60, 400)');
+    }
+    // handle left y axis
+    if (gdpMax) {
+      const yAxisScale = scaleLinear()
+        .domain([0, gdpMax])
+        .range([height, 0]);
+
+      const yAxis = axisLeft(yAxisScale);
+
+      svgNode
+        .append('g')
+        .call(yAxis)
+        .attr('id', 'y-axis')
+        .attr('transform', 'translate(60, 0)');
     }
   };
   render() {
